4a21bdc80e76f7301d4c026b0a727b529c235380
Murphy Amos <46387288+MurphyAmos@users.noreply.github.com>
2025-05-08 02:04:17 -0500

    Add files via upload
    This Is a api request from gemini that gives us common attacks on open ports

---
+++ GemResponse.py
@@ -0,0 +1,22 @@
+from google import genai
+from google.genai import types
+from google.genai.types import Tool, GoogleSearch, GenerateContentConfig
+
+class PortVulnerabilities:
+    def GemResponses(ports):
+        client = genai.Client(
+            api_key = "API_KEY"
+        )
+
+        googleSearch = Tool(google_search = GoogleSearch())
+        response = client.models.generate_content_stream(
+            model= "gemini-2.5-flash-preview-04-17",
+            contents = [f"You are a redHat cyberSec engineer with descriptive answers. list 3 Vulnerabilities for each port:", ports, "; make each index no more than 50 words, take away all warnings. You Will not introduce yourself in this prompt"],
+            config = types.GenerateContentConfig(
+                thinking_config=types.ThinkingConfig(thinking_budget=4096),
+                temperature = .2
+                )
+        )
+        for x in response:
+            print(x.text,end="")
+        print("")
